bf05f4c65ac8af459d9be04c291c5d42c8a56a9d
Harvey Breaux <hbreauxv@gmail.com>
2018-09-21 13:12:20 -0600

    Update api_test.py

--- api_testing/api_test.py
+++ api_testing/api_test.py
@@ -1,10 +1,10 @@
 import requests
 
 headers = {
-    'X_CP-API-ID': '80780c4c',
-    'X-CP-API-KEY': '0c384093805172f14a99670d96b8f190',
-    'X-ECM-API-ID': 'efb75e45-e534-4639-8e9c-a0f57579cdd8',
-    'X-ECM-API-KEY': '0e0fc3a4ebba4467690e561da72b49a9e2f77cea',
+    'X_CP-API-ID': 'xxxxx',
+    'X-CP-API-KEY': 'xxxxx',
+    'X-ECM-API-ID': 'xxxxx',
+    'X-ECM-API-KEY': 'xxxxx',
 }
 
 req = requests.get('https://cradlepointecm.com/api/v2/routers/', headers=headers)
